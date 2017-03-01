304096859c18d5d6f930ccca2f907d930e652780
Dimitre Bogdanov <dimitre_7@hotmail.com>
2017-03-01 18:52:51 -0500

    Structure for some of the search and sort algorithms

---
+++ Tutorial/src/algorithms/Search.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+public class Search {
+
+	private Search(){
+		
+	}
+	
+	public static int binary(Object[] array, Object find){
+		return -1;
+	}
+	
+	public static int linear(Object[] array, Object find){
+		return -1;
+	}
+}
---
+++ Tutorial/src/algorithms/Sorting.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+public class Sorting {
+
+	private Sorting() {
+	}
+
+	public static int[] radix(int[] array) {
+		return null;
+	}
+
+	public static Object[] merge(Object[] array) {
+		return null;
+	}
+
+	public static Object[] quick(Object[] array) {
+		return null;
+	}
+
+	public static Object[] bubble(Object[] array) {
+		return null;
+	}
+
+	public static Object[] selection(Object[] array) {
+		return null;
+	}
+
+	public static Object[] insertion(Object[] array) {
+		return null;
+	}
+
+	public static Object[] heap(Object[] array) {
+		return null;
+	}
+
+	public static Object[] tree(Object[] array) {
+		return null;
+	}
+
+}

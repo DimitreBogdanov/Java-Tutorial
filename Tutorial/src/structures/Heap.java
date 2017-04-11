ae255d54d14e96e564f28ba2f68992b0c5d42d8e
Dimitre Bogdanov <dimitre_7@hotmail.com>
2017-04-10 23:59:25 -0400

    Heap
    Beginning of implementation

--- Tutorial/src/structures/Heap.java
+++ Tutorial/src/structures/Heap.java
@@ -1,8 +1,20 @@
 package structures;
 
+import java.util.List;
+
 public class Heap<E extends Comparable<E>> {
 
+	private List<E> data;
+
+	// Left child 2n
+	// Right child 2n + 1
+	// Parent is n/2
+
 	public Heap() {
+
+		data = new ArrayList<E>();
+		// Add the empty space as the first index for the sake of implementation
+		data.add(null);
 
 	}
 
